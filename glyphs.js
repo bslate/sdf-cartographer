5205f70b4476be6f8ff9073ce1bd311aa62bd7bf
Donald Ness <donald.ness@gmail.com>
2016-03-07 10:48:40 -0700

    Standard format glyphs.js

--- glyphs.js
+++ glyphs.js
@@ -1,83 +1,83 @@
-'use strict';
+'use strict'
 
 module.exports = Glyphs
 
-function Glyphs(buffer, end) {
-  
-  this.stacks = {};
-  
-  var buffer, val, tag;
+function Glyphs (buffer, end) {
+  this.stacks = {}
 
+  var buffer
+  var val, tag
+  
   if (typeof end === 'undefined') {
     end = buffer.length
   }
 
   while (buffer.pos < end) {
-    val = buffer.readVarint();
-    tag = val >> 3;
+    val = buffer.readVarint()
+    tag = val >> 3
     if (tag == 1) {
-      var fontstack = readFontstack(buffer);
-      this.stacks[fontstack.name] = fontstack;
+      var fontstack = readFontstack(buffer)
+      this.stacks[fontstack.name] = fontstack
     } else {
-      buffer.skip(val);
+      buffer.skip(val)
     }
   }
 }
 
-function readFontstack(buffer) {
-  var fontstack = { glyphs: {} };
+function readFontstack (buffer) {
+  var fontstack = { glyphs: {} }
 
-  var bytes = buffer.readVarint();
-  var val, tag;
-  var end = buffer.pos + bytes;
+  var bytes = buffer.readVarint()
+  var val, tag
+  var end = buffer.pos + bytes
   while (buffer.pos < end) {
-    val = buffer.readVarint();
-    tag = val >> 3;
+    val = buffer.readVarint()
+    tag = val >> 3
 
     if (tag == 1) {
-      fontstack.name = buffer.readString();
+      fontstack.name = buffer.readString()
     } else if (tag == 2) {
-      var range = buffer.readString();
-      fontstack.range = range;
+      var range = buffer.readString()
+      fontstack.range = range
     } else if (tag == 3) {
-      var glyph = readGlyph(buffer);
-      fontstack.glyphs[glyph.id] = glyph;
+      var glyph = readGlyph(buffer)
+      fontstack.glyphs[glyph.id] = glyph
     } else {
-      buffer.skip(val);
+      buffer.skip(val)
     }
   }
 
-  return fontstack;
-};
+  return fontstack
+}
 
-function readGlyph(buffer) {
-  var glyph = {};
+function readGlyph (buffer) {
+  var glyph = {}
 
-  var bytes = buffer.readVarint();
-  var val, tag;
-  var end = buffer.pos + bytes;
+  var bytes = buffer.readVarint()
+  var val, tag
+  var end = buffer.pos + bytes
   while (buffer.pos < end) {
-    val = buffer.readVarint();
-    tag = val >> 3;
+    val = buffer.readVarint()
+    tag = val >> 3
 
     if (tag == 1) {
-      glyph.id = buffer.readVarint();
+      glyph.id = buffer.readVarint()
     } else if (tag == 2) {
-      glyph.bitmap = buffer.readBytes();
+      glyph.bitmap = buffer.readBytes()
     } else if (tag == 3) {
-      glyph.width = buffer.readVarint();
+      glyph.width = buffer.readVarint()
     } else if (tag == 4) {
-      glyph.height = buffer.readVarint();
+      glyph.height = buffer.readVarint()
     } else if (tag == 5) {
-      glyph.left = buffer.readSVarint();
+      glyph.left = buffer.readSVarint()
     } else if (tag == 6) {
-      glyph.top = buffer.readSVarint();
+      glyph.top = buffer.readSVarint()
     } else if (tag == 7) {
-      glyph.advance = buffer.readVarint();
+      glyph.advance = buffer.readVarint()
     } else {
-      buffer.skip(val);
+      buffer.skip(val)
     }
   }
 
-  return glyph;
-};
+  return glyph
+}
